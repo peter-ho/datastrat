6d5c9efae64d959162066319b2fc9325850f6250
peter.ho <peter-ho@live.com>
2019-06-26 11:10:31 -0500

    moved etl project to etl, added configuration files

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+scp -P 2272 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null config/application.properties root@192.168.1.227:/home/cloudera/workspace/config
+scp -P 2272 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null config/msft/gaming/gaming.properties root@192.168.1.227:/home/cloudera/workspace/config/msft/gaming
--- etl/src/main/scala/util/ConfLoader.scala
+++ etl/src/main/scala/util/ConfLoader.scala
@@ -9,7 +9,7 @@ import scala.collection.JavaConverters._
 
 object ConfLoader {
 
-  /** Read configuration file with a given file path resolving the entries with the given envirnoment and organization
+  /** Read configuration file(s) set up within the application given a specified environment, organization and area
     * @example 1
     *          {{{apply("dev", "msft", "user.activity")}}}
     * @param env environment identifier: dev, tst, uat, prd
@@ -23,22 +23,21 @@ object ConfLoader {
       val confBasePth = s"/$env/$org/etl/config/*.properties"
       val confPth = s"/$env/$org/etl/config/$ara/*.properties"
 
-    val fs = FileSystem.get(getHadoopConfiguration)
-    println(s" === PostWriteSetup for $tgtTblNm $rfrshNbr $hdfsPath $cols")
-    val bsPth = getTableHdfsPath(dbKey, tblNm)
-    println(s"   attempt clean up of $bsPth")
-    fs.listStatus(new Path(bsPth)).filter(_.isDir).filter(x => fs.listStatus(x.getPath).size == 0)
-      .foreach(x => {
-        print(s"    Trying to delete: ${x.getPath} : ")
-        println(if (fs.delete(x.getPath, false)) "Success" else "Fail")
+//ClassLoader.getSystemResourceAsStream("application.properties")
 
-      val props = loadFromFile(filepth)
-      if (props != null) {
-        props.stringPropertyNames().asScala.toList.foreach(
-          propName => mapp.put(propName, props.getProperty(propName)
-            .replace("$env", env).replace("$org", org).replace("$ara", ara)
-            + (if (propName.startsWith("db-")) "." else "")))
-      }
+      val fs = FileSystem.get(getHadoopConfiguration)
+      val load = (p:String, m:HashMap[String,String]) => fs.globStatus(new Path(p))
+        .filter(x => x.isFile && x.getLen > 0).map(_.getPath)
+        .foreach(x => {
+          val props = loadFromFile(x)
+          if (props != null) {
+            props.stringPropertyNames().asScala.toList.foreach(
+            propName => m.put(propName, props.getProperty(propName)
+              .replace("$env", env).replace("$org", org).replace("$ara", ara)
+              + (if (propName.startsWith("db.")) "." else "")))
+          }
+        }
+      load(
     } catch {
       case e:Exception => e.printStackTrace()
     }
@@ -52,10 +51,11 @@ object ConfLoader {
     * @param env environment identifier: dev, tst, uat, prd
     * @param org organization id identifing a tenant to be separated with others
     * @param ara subject area of the etl configuration
+    * @param fs FileSystem associated to the current Hadoop/Spark instance
     * @param filepth absolute path of the configuration file to be loaded
     * @return map with key value pair that matches configuration entries within the given configuration file
     */
-  def loadConfFromFile(env: String, org: String, ara: String, filepth:String):HashMap[String,String] = {
+  def loadConfFromFile(env:String, org:String, ara:String, fs:FileSystem, filepth:Path):HashMap[String,String] = {
     val mapp: HashMap[String,String] = HashMap.empty[String,String]
     try {
       println(s" ... reading from configuration file in hdfs: $filepth")
@@ -76,10 +76,11 @@ object ConfLoader {
   /** Reads a java properties file to a [[java.util.Properties]]
     * @example 1
     *          {{{loadFromFile("hdfs:///dev/org1/app1/config/app.properties")}}}
+    * @param fs FileSystem associated to the current Hadoop/Spark instance
     * @param filepth absolute path of the properties file to be read
     * @return an instance of [[java.util.Properties]] with the key value pairs in the properties file
     */
-  def loadFromFile(filepth: String):Properties = {
+  def loadFromFile(fs:FileSystem, filepth:Path):Properties = {
     var input:InputStream = null
     try {
       val pth = new Path(filepth)
